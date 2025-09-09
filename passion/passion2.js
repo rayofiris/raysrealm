0b7d13fed0125d69cccae4d6540766c984678bbd
ray burnette <rayburnette83@gmail.com>
2025-09-09 15:49:14 -0400

    popups and passion1 structure change

--- passion/passion2.js
+++ passion/passion2.js
@@ -1,6 +1,137 @@
-window.onload=function(){setTimeout(showPopup,4000)};
+
+
+window.onload=function(){setTimeout(showPopup,3000)};
 
 function showPopup()
 {
-    alert("I am an alert box!");
+    let popup1= document.getElementById("popup1");
+    popup1.style.visibility="visible";
+
+}
+
+function hideOne()
+{
+    let popup1= document.getElementById("popup1");
+    popup1.style.visibility="hidden";
+}
+
+window.addEventListener('load', function(){setTimeout(showPopuptwo,13000)});
+
+function showPopuptwo()
+{
+    let popup2= document.getElementById("popup2")
+    popup2.style.visibility="visible";
+}
+
+function hideTwo()
+{
+    let popup2= document.getElementById("popup2");
+    popup2.style.visibility="hidden";
+}
+
+window.addEventListener('load', function(){this.setTimeout(showPopupthree, 23000)});
+
+function showPopupthree()
+{
+    let popup3= document.getElementById("popup6");
+    popup3.style.visibility="visible";
+}
+
+function hideThree()
+{
+    let popup3= document.getElementById("popup6");
+    popup3.style.visibility="hidden";
+}
+
+window.addEventListener('load', function(){this.setTimeout(showPopupfour, 36000)});
+
+function showPopupfour()
+{
+    let popup4= document.getElementById("popup4");
+    popup4.style.visibility="visible";
+}
+
+function showFourpointfive()
+{
+    let popup4point5= document.getElementById("popup4point5");
+    popup4point5.style.visibility="visible";
+}
+
+function hideFour()
+{
+    let popup4= document.getElementById("popup4");
+    popup4.style.visibility="hidden";
+    let popup4point5= document.getElementById("popup4point5");
+    popup4point5.style.visibility="hidden";
+
+}
+
+window.addEventListener('load', function(){this.setTimeout(showPopupfive, 51000)});
+
+function showPopupfive()
+{
+    let popup5= document.getElementById("popup7");
+    popup5.style.visibility="visible";
+}
+
+function hideFive()
+{
+    let popup5= document.getElementById("popup7");
+    popup5.style.visibility="hidden";
+}
+
+window.addEventListener('load', function(){this.setTimeout(showPopupsix, 61000)});
+
+function showPopupsix()
+{
+    let popup6= document.getElementById("popup3");
+    popup6.style.visibility="visible";
+}
+
+function hideSix()
+{
+    let popup6= document.getElementById("popup3");
+    popup6.style.visibility="hidden";
+}
+
+window.addEventListener('load', function(){this.setTimeout(showPopupseven, 73000)});
+
+function showPopupseven()
+{
+    let popup7= document.getElementById("popup8");
+    popup7.style.visibility="visible";
+}
+
+function hideSeven()
+{
+    let popup7= document.getElementById("popup8");
+    popup7.style.visibility="hidden";
+}
+
+window.addEventListener('load', function(){this.setTimeout(showPopupeight, 87000)});
+
+function showPopupeight()
+{
+    let popup8= document.getElementById("popup5");
+    popup8.style.visibility="visible";
+}
+
+function hideEight()
+{
+    let popup8= document.getElementById("popup5");
+    popup8.style.visibility="hidden";
+}
+
+window.addEventListener('load', function(){this.setTimeout(showPopupnine, 97000)});
+
+function showPopupnine()
+{
+    let popup9= document.getElementById("popup9");
+    popup9.style.visibility="visible";
+}
+
+function hideNine()
+{
+    let popup9= document.getElementById("popup9");
+    popup9.style.visibility="hidden";
 }
