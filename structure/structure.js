ec9576674aa36ce29f528298cf81bd10b5cb94de
ray burnette <rayburnette83@gmail.com>
2025-09-18 14:53:24 -0400

    structure

---
+++ structure/structure.js
@@ -0,0 +1,227 @@
+function showPlay1() 
+{
+    let play1= document.querySelector(".play1");
+    let note1= document.querySelector(".note1");  
+    play1.style.visibility="visible";
+    note1.addEventListener("click", event => {
+if (play1.style.visibility ==="hidden") {
+ play1.style.visibility = "visible";}
+else{ play1.style.visibility ="hidden"}
+});
+}
+
+function showPlay2() 
+{
+    let play2= document.querySelector(".play2");
+    let note2= document.querySelector(".note2");  
+    play2.style.visibility="visible";
+    note2.addEventListener("click", event => {
+if (play2.style.visibility ==="hidden") {
+ play2.style.visibility = "visible";}
+else{ play2.style.visibility ="hidden"}
+});
+}
+
+function showPlay3() 
+{
+    let play3= document.querySelector(".play3");
+    let note3= document.querySelector(".note3");  
+    play3.style.visibility="visible";
+    note3.addEventListener("click", event => {
+if (play3.style.visibility ==="hidden") {
+ play3.style.visibility = "visible";}
+else{ play3.style.visibility ="hidden"}
+});
+}
+
+function showPlay4() 
+{
+    let play4= document.querySelector(".play4");
+    let note4= document.querySelector(".note4");  
+    play4.style.visibility="visible";
+    note4.addEventListener("click", event => {
+if (play4.style.visibility ==="hidden") {
+ play4.style.visibility = "visible";}
+else{ play4.style.visibility ="hidden"}
+});
+}
+
+function showPlay5() 
+{
+    let play5= document.querySelector(".play5");
+    let note5= document.querySelector(".note5");  
+    play5.style.visibility="visible";
+    note5.addEventListener("click", event => {
+if (play5.style.visibility ==="hidden") {
+ play5.style.visibility = "visible";}
+else{ play5.style.visibility ="hidden"}
+});
+}
+
+function showPlay6() 
+{
+    let play6= document.querySelector(".play6");
+    let note6= document.querySelector(".note6");  
+    play6.style.visibility="visible";
+    note6.addEventListener("click", event => {
+if (play6.style.visibility ==="hidden") {
+ play6.style.visibility = "visible";}
+else{ play6.style.visibility ="hidden"}
+});
+}
+
+function showPlay7() 
+{
+    let play7= document.querySelector(".play7");
+    let note7= document.querySelector(".note7");  
+    play7.style.visibility="visible";
+    note7.addEventListener("click", event => {
+if (play7.style.visibility ==="hidden") {
+ play7.style.visibility = "visible";}
+else{ play7.style.visibility ="hidden"}
+});
+}
+
+function showPlay8() 
+{
+    let play8= document.querySelector(".play8");
+    let note8= document.querySelector(".note8");  
+    play8.style.visibility="visible";
+    note8.addEventListener("click", event => {
+if (play8.style.visibility ==="hidden") {
+ play8.style.visibility = "visible";}
+else{ play8.style.visibility ="hidden"}
+});
+}
+
+function showPlay9() 
+{
+    let play9= document.querySelector(".play9");
+    let note9= document.querySelector(".note9");  
+    play9.style.visibility="visible";
+    note9.addEventListener("click", event => {
+if (play9.style.visibility ==="hidden") {
+ play9.style.visibility = "visible";}
+else{ play9.style.visibility ="hidden"}
+});
+}
+
+function showPlay10() 
+{
+    let play10= document.querySelector(".play10");
+    let note10= document.querySelector(".note10");  
+    play10.style.visibility="visible";
+    note10.addEventListener("click", event => {
+if (play10.style.visibility ==="hidden") {
+ play10.style.visibility = "visible";}
+else{ play10.style.visibility ="hidden"}
+});
+}
+
+function showPlay11() 
+{
+    let play11= document.querySelector(".play11");
+    let note11= document.querySelector(".note11");  
+    play11.style.visibility="visible";
+    note11.addEventListener("click", event => {
+if (play11.style.visibility ==="hidden") {
+ play11.style.visibility = "visible";}
+else{ play11.style.visibility ="hidden"}
+});
+}
+
+function showPlay12() 
+{
+    let play12= document.querySelector(".play12");
+    let note12= document.querySelector(".note12");  
+    play12.style.visibility="visible";
+    note12.addEventListener("click", event => {
+if (play12.style.visibility ==="hidden") {
+ play12.style.visibility = "visible";}
+else{ play12.style.visibility ="hidden"}
+});
+}
+
+function showPlay13() 
+{
+    let play13= document.querySelector(".play13");
+    let note13= document.querySelector(".note13");  
+    play13.style.visibility="visible";
+    note13.addEventListener("click", event => {
+if (play13.style.visibility ==="hidden") {
+ play13.style.visibility = "visible";}
+else{ play13.style.visibility ="hidden"}
+});
+}
+
+function showPlay14() 
+{
+    let play14= document.querySelector(".play14");
+    let note14= document.querySelector(".note14");  
+    play14.style.visibility="visible";
+    note14.addEventListener("click", event => {
+if (play14.style.visibility ==="hidden") {
+ play14.style.visibility = "visible";}
+else{ play14.style.visibility ="hidden"}
+});
+}
+
+function showPlay15() 
+{
+    let play15= document.querySelector(".play15");
+    let note15= document.querySelector(".note15");  
+    play15.style.visibility="visible";
+    note15.addEventListener("click", event => {
+if (play15.style.visibility ==="hidden") {
+ play15.style.visibility = "visible";}
+else{ play15.style.visibility ="hidden"}
+});
+}
+
+function showPlay16() 
+{
+    let play16= document.querySelector(".play16");
+    let note16= document.querySelector(".note16");  
+    play16.style.visibility="visible";
+    note16.addEventListener("click", event => {
+if (play16.style.visibility ==="hidden") {
+ play16.style.visibility = "visible";}
+else{ play16.style.visibility ="hidden"}
+});
+}
+
+function showPlay17() 
+{
+    let play17= document.querySelector(".play17");
+    let note17= document.querySelector(".note17");  
+    play17.style.visibility="visible";
+    note17.addEventListener("click", event => {
+if (play17.style.visibility ==="hidden") {
+ play17.style.visibility = "visible";}
+else{ play17.style.visibility ="hidden"}
+});
+}
+
+function showPlay18() 
+{
+    let play18= document.querySelector(".play18");
+    let note18= document.querySelector(".note18");  
+    play18.style.visibility="visible";
+    note18.addEventListener("click", event => {
+if (play18.style.visibility ==="hidden") {
+ play18.style.visibility = "visible";}
+else{ play18.style.visibility ="hidden"}
+});
+}
+
+function showPlay19() 
+{
+    let play19= document.querySelector(".play19");
+    let note19= document.querySelector(".note19");  
+    play19.style.visibility="visible";
+    note19.addEventListener("click", event => {
+if (play19.style.visibility ==="hidden") {
+ play19.style.visibility = "visible";}
+else{ play19.style.visibility ="hidden"}
+});
+}
