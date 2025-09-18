7e88a4493e3df11db377f2234bdd11ccb8c2cc1d
ray burnette <rayburnette83@gmail.com>
2025-09-18 17:38:16 -0400

    audio 2

--- structure/structure.js
+++ structure/structure.js
@@ -21,6 +21,7 @@ function showPlay2()
     let note2= document.querySelector(".note2");  
     let gnatural= document.getElementById("gnatural");
     play2.style.visibility="visible";
+    gnatural.play();
     note2.addEventListener("click", event => {
 if (play2.style.visibility ==="hidden") {
  play2.style.visibility = "visible";
@@ -37,6 +38,7 @@ function showPlay3()
     let note3= document.querySelector(".note3");  
      let fsharp= document.getElementById("fsharp"); 
     play3.style.visibility="visible";
+    fsharp.play();
     note3.addEventListener("click", event => {
 if (play3.style.visibility ==="hidden") {
  play3.style.visibility = "visible";
@@ -53,6 +55,7 @@ function showPlay4()
     let note4= document.querySelector(".note4"); 
     let dnatural= document.getElementById("dnatural"); 
     play4.style.visibility="visible";
+    dnatural.play();
     note4.addEventListener("click", event => {
 if (play4.style.visibility ==="hidden") {
  play4.style.visibility = "visible";
@@ -69,6 +72,7 @@ function showPlay5()
     let note5= document.querySelector(".note5"); 
     let bnatural= document.getElementById("bnatural");  
     play5.style.visibility="visible";
+    bnatural.play();
     note5.addEventListener("click", event => {
 if (play5.style.visibility ==="hidden") {
  play5.style.visibility = "visible";
@@ -85,6 +89,7 @@ function showPlay6()
     let note6= document.querySelector(".note6"); 
     let gnatural= document.getElementById("gnatural"); 
     play6.style.visibility="visible";
+    gnatural.play();
     note6.addEventListener("click", event => {
 if (play6.style.visibility ==="hidden") {
  play6.style.visibility = "visible";
@@ -101,6 +106,7 @@ function showPlay7()
     let note7= document.querySelector(".note7");  
      let fsharp= document.getElementById("fsharp"); 
     play7.style.visibility="visible";
+    fsharp.play();
     note7.addEventListener("click", event => {
 if (play7.style.visibility ==="hidden") {
  play7.style.visibility = "visible";
@@ -117,6 +123,7 @@ function showPlay8()
     let note8= document.querySelector(".note8"); 
     let dnatural= document.getElementById("dnatural");  
     play8.style.visibility="visible";
+    dnatural.play();
     note8.addEventListener("click", event => {
 if (play8.style.visibility ==="hidden") {
  play8.style.visibility = "visible";
@@ -133,6 +140,7 @@ function showPlay9()
     let note9= document.querySelector(".note9");  
     let bnatural= document.getElementById("bnatural"); 
     play9.style.visibility="visible";
+    bnatural.play();
     note9.addEventListener("click", event => {
 if (play9.style.visibility ==="hidden") {
  play9.style.visibility = "visible";
@@ -149,6 +157,7 @@ function showPlay10()
     let note10= document.querySelector(".note10");
     let gnatural= document.getElementById("gnatural");  
     play10.style.visibility="visible";
+    gnatural.play();
     note10.addEventListener("click", event => {
 if (play10.style.visibility ==="hidden") {
  play10.style.visibility = "visible";
@@ -163,6 +172,7 @@ function showPlay11()
     let note11= document.querySelector(".note11"); 
      let fsharp= document.getElementById("fsharp");  
     play11.style.visibility="visible";
+    fsharp.play();
     note11.addEventListener("click", event => {
 if (play11.style.visibility ==="hidden") {
  play11.style.visibility = "visible";
@@ -179,6 +189,7 @@ function showPlay12()
     let note12= document.querySelector(".note12");
     let dnatural= document.getElementById("dnatural");   
     play12.style.visibility="visible";
+    dnatural.play();
     note12.addEventListener("click", event => {
 if (play12.style.visibility ==="hidden") {
  play12.style.visibility = "visible";
@@ -195,6 +206,7 @@ function showPlay13()
     let note13= document.querySelector(".note13");  
     let bnatural= document.getElementById("bnatural"); 
     play13.style.visibility="visible";
+    bnatural.play();
     note13.addEventListener("click", event => {
 if (play13.style.visibility ==="hidden") {
  play13.style.visibility = "visible";
@@ -211,6 +223,7 @@ function showPlay14()
     let note14= document.querySelector(".note14");
     let gnatural= document.getElementById("gnatural");  
     play14.style.visibility="visible";
+    gnatural.play();
     note14.addEventListener("click", event => {
 if (play14.style.visibility ==="hidden") {
  play14.style.visibility = "visible";
@@ -227,6 +240,7 @@ function showPlay15()
     let note15= document.querySelector(".note15");  
      let fsharp= document.getElementById("fsharp"); 
     play15.style.visibility="visible";
+    fsharp.play();
     note15.addEventListener("click", event => {
 if (play15.style.visibility ==="hidden") {
  play15.style.visibility = "visible";
@@ -243,6 +257,7 @@ function showPlay16()
     let note16= document.querySelector(".note16"); 
     let dnatural= document.getElementById("dnatural");  
     play16.style.visibility="visible";
+    dnatural.play();
     note16.addEventListener("click", event => {
 if (play16.style.visibility ==="hidden") {
  play16.style.visibility = "visible";
@@ -259,6 +274,7 @@ function showPlay17()
     let note17= document.querySelector(".note17");
     let bnatural= document.getElementById("bnatural");   
     play17.style.visibility="visible";
+    bnatural.play();
     note17.addEventListener("click", event => {
 if (play17.style.visibility ==="hidden") {
  play17.style.visibility = "visible";
@@ -275,6 +291,7 @@ function showPlay18()
     let note18= document.querySelector(".note18");  
     let gnatural= document.getElementById("gnatural");
     play18.style.visibility="visible";
+    gnatural.play();
     note18.addEventListener("click", event => {
 if (play18.style.visibility ==="hidden") {
  play18.style.visibility = "visible";
@@ -291,6 +308,7 @@ function showPlay19()
     let note19= document.querySelector(".note19");
     let fsharp= document.getElementById("fsharp");  
     play19.style.visibility="visible";
+    fsharp.play();
     note19.addEventListener("click", event => {
 if (play19.style.visibility ==="hidden") {
  play19.style.visibility = "visible";
